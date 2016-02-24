220145874338bd7098ec733c10345bf3222f050c
velvetbabe <antonella.gutierrezcuentas@gmail.com>
2016-02-24 12:37:50 -0500

    responsive blog

---
+++ jquery.js
@@ -0,0 +1,19 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script>
+    $(function() {
+
+        var btn_movil = $('#nav-mobile'),
+            menu = $('#menu').find('ul');
+
+        // Al dar click agregar/quitar clases que permiten el despliegue del menú
+        btn_movil.on('click', function (e) {
+            e.preventDefault();
+
+            var el = $(this);
+
+            el.toggleClass('nav-active');
+            menu.toggleClass('open-menu');
+        })
+
+    });
+</script>
